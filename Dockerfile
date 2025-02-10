54ba9d431427c71449f755cf74044bc80351950c
rsmullick <shahriar.mullick@valpo.edu>
2025-02-11 00:35:47 +0600

    docker files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,17 @@
-FROM ubuntu:latest
-LABEL authors="shahr"
+# Use an official OpenJDK 23 image as the base image
+FROM openjdk:23-jdk-slim
 
-ENTRYPOINT ["top", "-b"]
+# Set the working directory inside the container
+WORKDIR /app
+
+# Define a build argument for the JAR file path (default to build/libs)
+ARG JAR_PATH="build/libs/power-plant-0.0.1-SNAPSHOT.jar"
+
+# Copy the JAR file from the specified path (default or custom)
+COPY ${JAR_PATH} /app/power-plant.jar
+
+# Expose the port the application will run on
+EXPOSE 8080
+
+# Command to run the Spring Boot application
+CMD ["java", "-jar", "/app/power-plant.jar"]
--- src/test/java/com/example/powerplant/service/SearchServiceTest.java
+++ src/test/java/com/example/powerplant/service/SearchServiceTest.java
@@ -2,9 +2,11 @@
 package com.example.powerplant.service;
 
 
+import com.example.powerplant.entity.PowerPlantEntity;
 import com.example.powerplant.payload.response.PowerPlantRegisterResponse;
 import com.example.powerplant.payload.response.SearchResponse;
 import com.example.powerplant.repository.PowerPlantRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -15,7 +17,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -35,13 +41,16 @@ class SearchServiceTest {
     @Test
     void testGetNamesAndStatistics() {
         // Prepare mock data
-        PowerPlantRegisterResponse plant1 = PowerPlantRegisterResponse.builder().name( "Plant1").postcode( 123).capacity( 1000L).build();
-        PowerPlantRegisterResponse plant2 = PowerPlantRegisterResponse.builder().name( "Plant2").postcode( 123).capacity( 2000L).build();
-        PowerPlantRegisterResponse plant3 = PowerPlantRegisterResponse.builder().name( "Plant3").postcode( 123).capacity( 1500L).build();
+        PowerPlantRegisterResponse plant1 = PowerPlantRegisterResponse.builder().name("Plant1").postcode(123).capacity(1000L).build();
+        PowerPlantRegisterResponse plant2 = PowerPlantRegisterResponse.builder().name("Plant2").postcode(123).capacity(2000L).build();
+        PowerPlantRegisterResponse plant3 = PowerPlantRegisterResponse.builder().name("Plant3").postcode(123).capacity(1500L).build();
+        PowerPlantEntity plantEntity1 = PowerPlantEntity.builder().name("Plant1").postcode(123).capacity(1000L).build();
+        PowerPlantEntity plantEntity2 = PowerPlantEntity.builder().name("Plant2").postcode(123).capacity(2000L).build();
+        PowerPlantEntity plantEntity3 = PowerPlantEntity.builder().name("Plant3").postcode(123).capacity(1500L).build();
 
         // Mock repository to return the flux of power plants
-        when(powerPlantRepository.findByPostcodeAndCapacityRange(1000, 2000, 500L, 3000L))
-                .thenReturn(Flux.just(plant1, plant2, plant3));
+        when(powerPlantRepository.findByPostcodeAndCapacityRange(anyInt(), anyInt(), anyLong(), anyLong()))
+                .thenReturn(Flux.just(plantEntity1, plantEntity2, plantEntity3));
 
         // Execute the method to be tested
         Mono<SearchResponse> result = searchService.getNamesAndStatistics(500L, 3000L, 1000, 2000);
@@ -50,14 +59,26 @@ class SearchServiceTest {
         StepVerifier.create(result)
                 .assertNext(response -> {
                     assertNotNull(response);
-                    assertEquals(1000L, response.getMinCapacity()); // Min capacity
-                    assertEquals(2000L, response.getMaxCapacity()); // Max capacity
-                    assertEquals(1500L, response.getAverageCapacity(), 0.01); // Average capacity
-                    assertEquals(4500L, response.getSumCapacity()); // Sum of capacities
-                    assertEquals(3, response.getNames().size()); // Verify there are 3 names
-                    assertTrue(response.getNames().containsAll(List.of("Plant1", "Plant2", "Plant3"))); // Ensure names match
+                    Assertions.assertEquals(1000L, (long) response.getMinCapacity()); // Min capacity
+                    Assertions.assertEquals(2000L, response.getMaxCapacity()); // Max capacity
+                    Assertions.assertEquals(1500L, response.getAverageCapacity(), 0.01); // Average capacity
+                    Assertions.assertEquals(4500L, response.getTotalCapacity()); // Sum of capacities
+                    Assertions.assertEquals(3, response.getPowerPlants().size()); // Verify there are 3 names
+                    Assertions.assertTrue(response.getPowerPlants().containsAll(List.of("Plant1", "Plant2", "Plant3"))); // Ensure names match
                 })
                 .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testGetNamesAndStatistics_RepositoryReturnsNull() {
+        when(powerPlantRepository.findByPostcodeAndCapacityRange(1000, 2000, 500L, 3000L))
+                .thenReturn(null);  // Simulating null return
+
+        Mono<SearchResponse> result = searchService.getNamesAndStatistics(500L, 3000L, 1000, 2000);
+
+        StepVerifier.create(result)
+                .expectError(IllegalStateException.class)  // Expect an error if null was returned
                 .verify();
     }
 
@@ -74,11 +95,11 @@ class SearchServiceTest {
         StepVerifier.create(result)
                 .assertNext(response -> {
                     assertNotNull(response);
-                    assertEquals(Long.MAX_VALUE, response.getMinCapacity()); // No data, so max possible value
-                    assertEquals(Long.MIN_VALUE, response.getMaxCapacity()); // No data, so min possible value
-                    assertEquals(0L, response.getAverageCapacity(), 0.01); // Average should be 0
-                    assertEquals(0L, response.getSumCapacity()); // Sum should be 0
-                    assertTrue(response.getNames().isEmpty()); // No names should be there
+                    Assertions.assertEquals(Long.MAX_VALUE, response.getMinCapacity()); // No data, so max possible value
+                    Assertions.assertEquals(Long.MIN_VALUE, response.getMaxCapacity()); // No data, so min possible value
+                    Assertions.assertEquals(0L, response.getAverageCapacity(), 0.01); // Average should be 0
+                    Assertions.assertEquals(0L, response.getTotalCapacity()); // Sum should be 0
+                    Assertions.assertTrue(response.getPowerPlants().isEmpty()); // No names should be there
                 })
                 .expectComplete()
                 .verify();
